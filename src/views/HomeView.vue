44be1e4a13e2431a4c6bec0b7126d276f2e387d6
Nikolaychik <nikolay@gomobile.ru>
2022-11-26 17:57:05 +0300

    commit first ver

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <MainPage msg="Welcome to Your Vue.js App" />
+    <h1 v-if="data.length == 0">Загрузка...</h1>
+    <ul v-else >
+      <li v-for="(i,index) in data" :key="index">
+        <h3>{{i.name}}</h3>
+        <p>{{i.description}}</p>
+        <h4>{{i.price}} руб</h4>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { onMounted,ref } from 'vue'
+import axios from 'axios'
+import MainPage from '@/components/MainPage.vue'
+const data = ref([])
+onMounted(() => {
+  console.log('dasdas');
+  axios.get('https://script.google.com/macros/s/AKfycbxP2ychaEYefQ9p_V9liFboUH3Y5CiJsB_ujT5zcjSeQCz0GC6obZh7ECio0ulK2X1y/exec').then((el)=>{
+    console.log(el);
+    data.value = el.data
+  })
+})
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
